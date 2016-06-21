45f5a406bda73fd2f2bc4df88c6c1b9242be4482
Javier Sanchez Rois <jsanchez@gradiant.org>
2016-06-21 11:08:36 +0200

    Added files for e2 (finetuning with placesCNN)

---
+++ experiments/Emotion6/e2.finetuning_places205_emotion6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "$CAFFE_ROOT"
+if [ -z ${CAFFE_ROOT+x} ]; then 
+ echo "CAFFE_ROOT is unset"
+else 
+ echo "CAFFE_ROOT is set to '$CAFFE_ROOT'"
+ target_dir=$(dirname $0)
+ pushd $target_dir
+ caffe train -solver e2.solver.txt -weights $CAFFE_ROOT/extra_models/placesCNN_upgraded/places205CNN_iter_300000_upgraded.caffemodel -gpu all &> e2.log
+ popd
+fi
